49885d1b8b58c7bfd1d186b7d68c6f10e8a5e43e
Devin Mex <devin.mex@gmx.de>
2016-04-14 21:25:50 +0200

    Audio
    Audio Controller

---
+++ Unet/Scripts/Audio/AudioControler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioControler : MonoBehaviour {
+	
+	public AudioClip dying;
+	public AudioClip rifleShot;
+	public AudioClip bombExplosion;
+	public AudioClip yalaBomb;
+	public AudioClip grenadeThrow;
+	public AudioClip pistolShot;
+
+	public AudioClip[] roundWinAudioClip;
+	public AudioClip[] roundLoseAudioClip;
+
+	// Use this for initialization
+	void dyingSound () {
+		
+	}
+
+}
